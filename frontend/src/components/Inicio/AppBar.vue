9ab19fa42d581ba61e2c84fb2ca25f6299c0c44f
felipe <felipe.milla.calquin@gmail.com>
2020-05-16 16:46:08 -0400

    Implementación del App-bar de la vista de inicio (#1)
    App-Bar de inicio implementada, pero botón de "inicio sesión" no despliega la ventana emergente correspondiente.

---
+++ frontend/src/components/Inicio/AppBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="AppBar">
+        <v-app-bar 
+        color="primary"
+
+        elevate-on-scroll 
+        :collapse=false
+        >
+        <div class="d-flex align-center">
+        <v-img 
+            class="shrink mr-2"
+            src="@/assets/Inicio/utalca.png"
+           
+            width="300"
+        />
+        </div>
+        <v-spacer></v-spacer>
+        <v-btn text rounded color="white" >
+            <h4 class="white--text">Inicio Sesión</h4>
+            <h4 class="primary--text">1</h4>
+            <v-icon large >mdi-account-circle</v-icon>
+            
+        </v-btn>
+        </v-app-bar>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppBar',
+    methods:{
+        
+    },
+    computed:{
+
+    },
+
+}
