7a51ebcc26d1a6666de07076f987aaef178289c3
Javiera Méndez <jaavi.mendez8@gmail.com>
2020-05-18 01:03:28 -0400

    Crear migración, modelo y controlador de usuario (#18)
    Modificación tabla User (según diagrama), modelo y  creación cascarón del controlador de usuarios.

---
+++ Backend/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//Cambiar después a UserController
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function createUser()
+    {
+
+    }
+}
