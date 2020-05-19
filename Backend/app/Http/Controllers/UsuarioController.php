9f5e121b50b1169bd89be188838edc3ba69dbbe7
Javiera Méndez <jaavi.mendez8@gmail.com>
2020-05-19 10:23:33 -0400

    Implementación controlador 'Usuario' - Create (#10)
    Implementación del método create en UsuarioController (Sirve para crear un usuario y a su vez almacenarlo en la BD)

--- Backend/app/Http/Controllers/UsuarioController.php
+++ Backend/app/Http/Controllers/UsuarioController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 //Cambiar después a UserController
 class UsuarioController extends Controller
 {
+    #Retorna listado de todos los usuarios
     public function index()
     {
 
     }
 
-    public function createUser()
+    #public function createUser(Request $request)
+    //Store
+    public function create(Request $request)
     {
+        /*
+        $request = new Request([
+            'nombre'   => 'Pepito',
+            'carrera' => '1',
+            'rol' => 'Administrador', 
+            'foto' => '1',
+            'email'=> 'pepito@hotmail.com',
+            'password' => 'papa123',
+        ]);*/
 
+        $user = new User();
+        $user ->nombre=$request->nombre;
+        $user ->carrera=$request->carrera;
+        $user ->rol=$request->rol;
+        $user ->foto=$request->foto;
+        $user ->email=$request->email;
+        $user ->password=$request->password;
+        $r = $user ->save();
+        dd($r);
+        //return $user ;
     }
 }
