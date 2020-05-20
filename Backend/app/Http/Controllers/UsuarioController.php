94c06e4e50cb6b74c83e5c7b16d9a29ee9a29c1d
pauloAlexisdp <p.paraguez.diaz@hotmail.cl>
2020-05-20 16:00:16 -0400

    usuario-update (#11)
    Implementación del metodo update del CRUD del usuario

--- Backend/app/Http/Controllers/UsuarioController.php
+++ Backend/app/Http/Controllers/UsuarioController.php
@@ -1,19 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\User;
 
 use Illuminate\Http\Request;
 
-//Cambiar después a UserController
 class UsuarioController extends Controller
 {
-    public function index()
+   
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
+        $usuario = User::find($id);
+        $usuario-> nombre = $request->nombre;
+        $usuario-> carrera = $request->carrera;
+        $usuario-> foto = $request->foto;
+        $usuario-> email = $request->email;
+        $usuario-> password =  $request->password;
+        $usuario-> save();//
+        return compact('usuario');//para indicar al frontend que se creo el objeto usuario, con los datos obtenidos del request
 
     }
 
-    public function createUser()
-    {
 
-    }
 }
