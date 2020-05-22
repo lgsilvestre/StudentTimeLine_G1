aa56c66ed8b07bbbd7a36ad5f56ff3ce4ad937fb
felipe <felipe.milla.calquin@gmail.com>
2020-05-22 19:55:13 -0400

    Ajustes de detalles con el backend del método login (#20)
    Ademas de ajustar los detalles con el backend para la correcta funcionalidad de este método, se agregó la redireccion de los usuarios según su tipo.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,29 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import router from '@/router'
 Vue.use(Vuex)
 
 
 export default new Vuex.Store({
   state: {
     status:'',
-    resultado: null
+    usuario: null
   },
   mutations: {
-    login(state,lista){
+    login(state,lista){//funcion de login
       console.log(lista.email);
       console.log(lista.pass);
       let post ={
         "email": lista.email,
         "password": lista.pass,
       };
-      var url = 'por definir';
+      var url = 'http://127.0.0.1:8000/api/v1/auth/login';
       axios.post(url,post).then((result)=>{
-        console.log(result.statusText);
-        state.status = result.statusText; 
-        state.resultado = result;
+        if (state.usuario.user.rol == "admin") {
+          //redireccionamiento hacia el usuario administrador
+          router.push({path: '/administrador'});
+        } else {
+          if (state.usuario.user.rol == "secretaria de escuela") {
+            //redireccionamiento hacia el usuario secretaria de escuela
+            router.push({path: '/secretariaEscuela'});
+          } else {
+            if (state.usuario.user.rol == "profesor") {
+              //redireccionamiento hacia el usuario profesor
+              router.push({path: '/profesor'});
+            } else {
+              //alerta a usuarios que no estan registrados, esto se solucionara la proxima semana
+
+            }
+          }
+        }
       });
-    }
+    },
+    unLogin(state){
+      //esta funcionalidad se agregara mas adelante cuando el backend tenga lista esta funcionalidad
+    },
   },
   actions: {
   },
