6de20a1040bda38ef33bd8bcaf12dfe1138ce77d
Javiera Méndez <jaavi.mendez8@gmail.com>
2020-05-24 17:14:56 -0400

    Implementar migración, modelo y seeder Carrera (#41)
    Se implementó el modelo, migración y seeder (para cargar las 7 carreras de la universidad a la BD).

--- Backend/database/migrations/2020_05_24_183313_create_carreras_table.php
+++ Backend/database/migrations/2020_05_24_183313_create_carreras_table.php
@@ -15,7 +15,9 @@ class CreateCarrerasTable extends Migration
     {
         Schema::create('carreras', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre',64);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
