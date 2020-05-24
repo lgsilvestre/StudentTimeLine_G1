b3d36f8ca70248299ad05c9ba44b2e8c80e62f6d
Matias Pizarro <pinkystylee98@gmail.com>
2020-05-24 13:37:59 -0400

    Implementar Body de la vista registro de usuarios (#24)
    @worlark Implementamos la vista del registro de usuario, que corresponde a una funcionalidad del Administrador. Por el momento esta vinculada a la vista inicial de este, en commits futuros de cambiara para llegar a ella a traves de la barra de navegacion.

--- frontend/src/views/Administrador.vue
+++ frontend/src/views/Administrador.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="Administrador">
     <AppBarGlobal/>      
+    <BodyRegistroUsuarios/>
+    <FooterGeneral/>
   </div>
 </template>
 
 <script>
 import AppBarGlobal from '@/components/Globales/AppBarGlobal.vue';
+import BodyRegistroUsuarios from '@/components/Administrador/RegistroUsuarios/BodyRegistroUsuarios.vue';
+import FooterGeneral from '@/components/Globales/FooterGeneral.vue';
 
 export default {
     name: 'Administrador',
     components: {
       AppBarGlobal,
+      BodyRegistroUsuarios,
+      FooterGeneral,
     },
 }
 </script>
