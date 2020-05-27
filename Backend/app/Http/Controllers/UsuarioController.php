fa5fce1d2c68017b1e5520a4809f59b6a8b5ee7f
Sebastian Ibarra <xebaelvemgador@gmail.com>
2020-05-27 16:02:38 -0400

    Manejo de errores #48
    Se emplementaron metodos anticaidas y responsivos, las clases que se trabajaron fueron el tokensColtroller y el de usuarioController. Mas adelante se pulira el mecanismo de identificacion de JSON

--- Backend/app/Http/Controllers/UsuarioController.php
+++ Backend/app/Http/Controllers/UsuarioController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Images;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Http\Request;
 use Validator;
+use Illuminate\Http\Request;
 use Image;
 
 class UsuarioController extends Controller
@@ -19,68 +19,129 @@ class UsuarioController extends Controller
         $this->middleware(['permission:delete user'], ['only' => 'delete']);
     }
 
-    #Retorna listado de todos los usuarios
+    /**
+     * Metodo que permite listar todos los usuarios del sistema
+     */
     public function index()
     {
-        $users = User::all();
-        #dd($users);
-        return $users;
+        try{
+            $users = User::all();
+            return $users;
+        //este catch permite responder directamente que problemas en la peticion SQL
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            return response()->json([
+                'success' => false,
+                'code' => 5,
+                'message' => 'Error al solicitar peticiones a la base de datos'], 401);
+        }
     }
 
+    /**
+     * Metodo que permite modificar un usuario de la base de datos
+     */
     public function update(Request $request, $id)
     {
-        $usuario = User::find($id);
-        $usuario-> nombre = $request->nombre;
-        $usuario-> carrera = $request->carrera;
-        $usuario-> foto = $request->foto;
-        $usuario-> email = $request->email;
-        $usuario-> password =  $request->password;
-        $usuario-> save();//
-        return compact('usuario');//para indicar al frontend que se creo el objeto usuario, con los datos obtenidos del request
+        //Se puede modificar el rol?
+        //Deberia
+        $credentials = $request->only('nombre', 'carrera', 'foto', 'email', 'password');
+        $validator = Validator::make($credentials, [
+            'nombre' => ['string', 'nullable'],
+            'carrera' => ['integer', 'nullable'],
+            'foto' => ['image','mimes:jpeg,png,jpg,gif,svg','max:2048','nullable'],
+            'email' => ['email', 'nullable'],
+            'password' => ['string', 'nullable']
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'code' => 1,
+                'message' => 'Error en datos ingresados',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        try{
+            // las siguientes validaciones es por el motivo que el administrador puede modificar todos o uno de los elementos
+            //esto espesificamente evita tener errores de modificaciones en la base de datos por elementos no nules
+            $usuario = User::find($id);
+            if($request->nombre!=null){
+                $usuario-> nombre = $request->nombre;
+            }
+            if($request->carrera!=null){
+                $usuario-> carrera = $request->carrera;
+            }
+            if($request->foto!=null){
+                if($request->hasfile('foto')){
+                    $imagen = $request->file('foto');
+                    $nombreImagen = time () . '.' . $imagen->getClientOriginalExtension();
+                    Image::make($imagen)->resize(400,400)->save( public_path('/uploads/imagenes/' . $nombreImagen));
+                    $usuario->foto=$nombreImagen;
+                }
+            }
+            if($request->email!=null){
+                $usuario-> email = $request->email;
+            }
+            if($request->password!=null){
+                $usuario-> password =  bcrypt($request->password);
+            }            
+            $usuario-> save();//
+            return compact('usuario');//para indicar al frontend que se creo el objeto usuario, con los datos obtenidos del request
+        //este catch permite responder directamente que problemas en la peticion SQL
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            return response()->json([
+                'success' => false,
+                'code' => 5,
+                'message' => 'Error al solicitar peticiones a la base de datos'], 401);
+        }
     }
 
-    //Lo creé para hacer el testing de index
+    /**
+     * Metodo que permite obtener un usuario de la base de datos
+     */
     public function show($id)
     {
-        $user = User::findOrFail($id);
-        return $user;
+        try{
+            $user = User::findOrFail($id);
+            return $user;
+        //este catch permite responder directamente que problemas en la peticion SQL
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            return response()->json([
+                'success' => false,
+                'code' => 5,
+                'message' => 'Error al solicitar peticiones a la base de datos'], 401);
+        }
     }
 
+    /**
+     * Metodo que permite crear un usuario en la base de datos
+     */
     public function store(Request $request)
-    {
-        /*
-        $request = new Request([
-            'nombre'   => 'Pepito',
-            'carrera' => '1',
-            'rol' => 'Administrador', 
-            'foto' => '1',
-            'email'=> 'pepito@hotmail.com',
-            'password' => 'papa123',
-        ]);*/
-            
+    {            
+        //validador que se encarga de revisar que los datos sean del tipo de dato que se solicito
+        //tambien verifica que vengan como por ejemplo el email y el password
         $validator = Validator::make($request->all(), [
             'nombre' => ['required','string'],
-            'carrera' => ['required'], //Cambiar lo de la foreign key dps
+            'carrera' => ['required', 'integer'], //Cambiar lo de la foreign key dps
             'rol' => ['required','string'], 
             'foto' => ['image','mimes:jpeg,png,jpg,gif,svg','max:2048','nullable'],
             'email'=> ['required','email'],
-            'password' => ['required'],
+            'password' => ['required' , 'string']
         ]);
-
-        if ($validator->fails()) 
-        {
-            //dd($request);
-            return response($validator->messages());
+        //respuesta cuando falla
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'code' => 1,
+                'message' => 'Error en datos ingresados',
+                'errors' => $validator->errors()
+            ], 422);
         }
-        else
-        {
+        try{
             $user = new User();
             $user ->nombre=$request->nombre;
             $user ->carrera=$request->carrera;
             $user ->rol=$request->rol;
             $user ->email=$request->email;
-            $user ->password=$request->password;
-
+            $user ->password=bcrypt($request->password);
             if($request->hasfile('foto'))
             {
                 $imagen = $request->file('foto');
@@ -92,18 +153,37 @@ class UsuarioController extends Controller
             {
                 $user ->foto=null;
             }
-            
             $r = $user->save();
+            $user->assignRole($request->rol);
             return compact('user');
-        }    
+
+        //este catch permite responder directamente que problemas en la peticion SQL
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            return response()->json([
+                'success' => false,
+                'code' => 5,
+                'message' => 'Error al solicitar peticiones a la base de datos'], 401);
+        }
+        //catch que se encarga en responder que paso en la sentencia sql
+ 
     }
     
+    /**
+     * Metodo que se encarga de eliminar un usuario con de la base de datos de manera logica
+     */
     public function destroy($id)
     {
-        $user = User::find($id);
-        $user->delete();
-        //dd($user);
-        //dd($user->delete_at);
-        return compact('user');
+        try{
+            $user = User::find($id);
+            $user->delete();
+            return compact('user');
+            
+        //catch que se encarga en responder que paso en la sentencia sql
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            return response()->json([
+                'success' => false,
+                'code' => 5,
+                'message' => 'Error al solicitar peticiones a la base de datos'], 401);
+        }
     }
 }
