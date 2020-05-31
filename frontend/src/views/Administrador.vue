ca2ab17678a08685e6cadc55719f209fe4a445ec
brayan <brayan.escobar@live.com>
2020-05-31 16:02:06 -0400

    Auto stash before merge of "brayan" and "origin/matias"

--- frontend/src/views/Administrador.vue
+++ frontend/src/views/Administrador.vue
@@ -3,8 +3,7 @@
     <AppBarGlobal/>  
     <BodyRegistroUsuarios/>
     <FooterGlobal/>
-    <Navegacion/> 
-    
+    <Navegacion/>
   </div>
 </template>
 
@@ -13,13 +12,14 @@ import AppBarGlobal from '@/components/Globales/AppBarGlobal.vue';
 import BodyRegistroUsuarios from '@/components/Administrador/RegistroUsuarios/BodyRegistroUsuarios.vue';
 import FooterGlobal from '@/components/Globales/FooterGlobal.vue';
 import Navegacion from '@/components/Administrador/Navegacion.vue';
+
+
 export default {
     components: {
       AppBarGlobal,
       BodyRegistroUsuarios,
       FooterGlobal,
       Navegacion,
-      
     },
 }
 </script>
