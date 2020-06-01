e179b7818b3146510547a46e28234a16433190b7
felipe <felipe.milla.calquin@gmail.com>
2020-05-31 21:13:42 -0400

    Implementar método para cambiar los datos del usuario (#50)
    Implementar método updateDatosUsuarioPerfil para que los usuarios puedan cambiar los datos que se les permite desde su perfil

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,6 +12,9 @@ export default new Vuex.Store({
     RCstatus:null,
     tk:null,
     drawelAdmin: false,
+    admin: false,
+    profesor: false,
+    secretariaEscuela: false,
     config:{
       headers:{
         Authorization: ''
@@ -30,9 +33,14 @@ export default new Vuex.Store({
           var url = 'http://127.0.0.1:8000/api/v1/auth/login';
           axios.post(url, post).then((result) => {
               state.usuario = result.data;
+              state.tk = 'Bearer '+ state.usuario.token;
+              state.config.headers.Authorization = state.tk;
               if (state.usuario.user.rol == "admin") {
                   //redireccionamiento hacia el usuario administrador
+                  state.admin=true;
                   router.push({ path: '/administrador' });
+                  
+                  
               } else {
                   if (state.usuario.user.rol == "secretaria de escuela") {
                       //redireccionamiento hacia el usuario secretaria de escuela
@@ -52,18 +60,37 @@ export default new Vuex.Store({
       unLogin(state){
         //esta funcionalidad se agregara mas adelante cuando el backend tenga lista esta funcionalidad
       },
-      registrarUsuario(state,nuevoUsuario){
+      
+      registrarUsuario(state,datosUsuario){
         console.log(state.tk);
         let post ={
           "foto": null,
+          "nombre": datosUsuario.nombre,
+          "carrera": datosUsuario.escuela,
+          "rol": datosUsuario.rol,
+          "email": datosUsuario.correo,
+          "password": datosUsuario.contrasena,
+        }
+        var url = 'http://127.0.0.1:8000/api/v1/User';
+        console.log(state.config);
+        axios.post(url,post,state.config)
+          .then((result)=>{
+            console.log(result.statusText);
+          });
+      },
+      updateDatosUsuarioPerfil(state,nuevoUsuario){
+        console.log(state.tk);
+        let put ={
           "nombre": nuevoUsuario.nombre,
-          "carrera": nuevoUsuario.escuela,
-          "rol": nuevoUsuario.rol,
+          "escuela": null,
+          "rol": null,
+          "foto": null,
           "email": nuevoUsuario.correo,
           "password": nuevoUsuario.contrasena,
         }
-        var url = 'http://127.0.0.1:8000/api/v1/User';
-        axios.post(url,post,state.config)
+        var url = 'http://127.0.0.1:8000/api/v1/usuario/'+state.usuario.data.user.id;
+        console.log(state.config);
+        axios.put(url,put,state.config)
           .then((result)=>{
             console.log(result.statusText);
           });
