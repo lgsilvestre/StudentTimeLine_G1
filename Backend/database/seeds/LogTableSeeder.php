59700df8d21776707b9d5f8d88946c6668bf93f2
Sebastian Ibarra <xebaelvemgador@gmail.com>
2020-07-19 22:43:36 -0400

    Manejo de errores #48
    seeder de log

---
+++ Backend/database/seeds/LogTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Log;
+class LogTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        Log::create([
+            'titulo' => "Inicializacion del sistema",
+            'descripcion' => json([
+                'success' => "true",
+            ]),
+            'usuario' => '1'
+        ]);
+        */
+    }
+}
