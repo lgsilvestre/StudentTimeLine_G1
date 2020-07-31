665e7bb09cf45a3f18dd245a78621a9d2823b74a
felipe <felipe.milla.calquin@gmail.com>
2020-07-31 16:40:35 -0400

    errores #195
    this

--- frontend/src/components/Globales/EstudiantesGlobal.vue
+++ frontend/src/components/Globales/EstudiantesGlobal.vue
@@ -848,10 +848,10 @@
                 this.$router.push({path:'/administrador/estudiantes/id='+item.id});
             } else {
                 if (this.$store.state.usuario.usuario.rol == "secretaria de escuela") {
-                    this.$router.push({path:'/secretariaEscuela/estudiantes/?id='+item.id});
+                    this.$router.push({path:'/secretariaEscuela/estudiantes/id='+item.id});
                 } else {
                     if (this.$store.state.usuario.usuario.rol == "profesor") {
-                        this.$router.push({path:'/profesor/estudiantes/?id='+item.id});
+                        this.$router.push({path:'/profesor/estudiantes/id='+item.id});
                     }
                 }
             }
