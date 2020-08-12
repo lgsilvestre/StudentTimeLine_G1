66414caa950bccd9909081fbb8898ddbdeb71c12
Sebastian Ibarra <xebaelvemgador@gmail.com>
2020-08-12 18:07:42 -0400

    Manejo de errores (#48)
    Se arreglo arreglo el correo de contactar

--- Backend/resources/views/emails/sendmailContactar.blade.php
+++ Backend/resources/views/emails/sendmailContactar.blade.php
@@ -1,2 +1,446 @@
-<h1>{{$details['descripcion']}}</h1>
-<h1>{{$details['usuario']}}</h1>
+<!doctype html>
+<html ⚡4email>
+ <head><meta charset="utf-8"><style amp4email-boilerplate>body{visibility:hidden}</style><script async src="https://cdn.ampproject.org/v0.js"></script><script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script> 
+   
+  <style amp-custom>
+@media only screen and (max-width:600px) {p, ul li, ol li, a { font-size:16px; line-height:150% } h1 { font-size:30px; text-align:center; line-height:120% } h2 { font-size:26px; text-align:center; line-height:120% } h3 { font-size:20px; text-align:center; line-height:120% } h1 a { font-size:30px } h2 a { font-size:26px } h3 a { font-size:20px } .es-menu td a { font-size:16px } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px } *[class="gmail-fix"] { display:none } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left } .es-m-txt-r amp-img { float:right } .es-m-txt-c amp-img { margin:0 auto } .es-m-txt-l amp-img { float:left } .es-button-border { display:block } a.es-button { font-size:20px; display:block; border-left-width:0px; border-right-width:0px } .es-btn-fw { border-width:10px 0px; text-align:center } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100% } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%; max-width:600px } .es-adapt-td { display:block; width:100% } .adapt-img { width:100%; height:auto } td.es-m-p0 { padding:0px } td.es-m-p0r { padding-right:0px } td.es-m-p0l { padding-left:0px } td.es-m-p0t { padding-top:0px } td.es-m-p0b { padding-bottom:0 } td.es-m-p20b { padding-bottom:20px } .es-mobile-hidden, .es-hidden { display:none } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { display:table-row; width:auto; overflow:visible; float:none; max-height:inherit; line-height:inherit } .es-desk-menu-hidden { display:table-cell } table.es-table-not-adapt, .esd-block-html table { width:auto } table.es-social { display:inline-block } table.es-social td { display:inline-block } }
+a[x-apple-data-detectors] {
+	color:inherit;
+	text-decoration:none;
+	font-size:inherit;
+	font-family:inherit;
+	font-weight:inherit;
+	line-height:inherit;
+}
+.es-desk-hidden {
+	display:none;
+	float:left;
+	overflow:hidden;
+	width:0;
+	max-height:0;
+	line-height:0;
+}
+s {
+	text-decoration:line-through;
+}
+body {
+	width:100%;
+	font-family:lato, "helvetica neue", helvetica, arial, sans-serif;
+}
+table {
+	border-collapse:collapse;
+	border-spacing:0px;
+}
+table td, html, body, .es-wrapper {
+	padding:0;
+	Margin:0;
+}
+.es-content, .es-header, .es-footer {
+	table-layout:fixed;
+	width:100%;
+}
+p, hr {
+	Margin:0;
+}
+h1, h2, h3, h4, h5 {
+	Margin:0;
+	line-height:120%;
+	font-family:lato, "helvetica neue", helvetica, arial, sans-serif;
+}
+.es-left {
+	float:left;
+}
+.es-right {
+	float:right;
+}
+.es-p5 {
+	padding:5px;
+}
+.es-p5t {
+	padding-top:5px;
+}
+.es-p5b {
+	padding-bottom:5px;
+}
+.es-p5l {
+	padding-left:5px;
+}
+.es-p5r {
+	padding-right:5px;
+}
+.es-p10 {
+	padding:10px;
+}
+.es-p10t {
+	padding-top:10px;
+}
+.es-p10b {
+	padding-bottom:10px;
+}
+.es-p10l {
+	padding-left:10px;
+}
+.es-p10r {
+	padding-right:10px;
+}
+.es-p15 {
+	padding:15px;
+}
+.es-p15t {
+	padding-top:15px;
+}
+.es-p15b {
+	padding-bottom:15px;
+}
+.es-p15l {
+	padding-left:15px;
+}
+.es-p15r {
+	padding-right:15px;
+}
+.es-p20 {
+	padding:20px;
+}
+.es-p20t {
+	padding-top:20px;
+}
+.es-p20b {
+	padding-bottom:20px;
+}
+.es-p20l {
+	padding-left:20px;
+}
+.es-p20r {
+	padding-right:20px;
+}
+.es-p25 {
+	padding:25px;
+}
+.es-p25t {
+	padding-top:25px;
+}
+.es-p25b {
+	padding-bottom:25px;
+}
+.es-p25l {
+	padding-left:25px;
+}
+.es-p25r {
+	padding-right:25px;
+}
+.es-p30 {
+	padding:30px;
+}
+.es-p30t {
+	padding-top:30px;
+}
+.es-p30b {
+	padding-bottom:30px;
+}
+.es-p30l {
+	padding-left:30px;
+}
+.es-p30r {
+	padding-right:30px;
+}
+.es-p35 {
+	padding:35px;
+}
+.es-p35t {
+	padding-top:35px;
+}
+.es-p35b {
+	padding-bottom:35px;
+}
+.es-p35l {
+	padding-left:35px;
+}
+.es-p35r {
+	padding-right:35px;
+}
+.es-p40 {
+	padding:40px;
+}
+.es-p40t {
+	padding-top:40px;
+}
+.es-p40b {
+	padding-bottom:40px;
+}
+.es-p40l {
+	padding-left:40px;
+}
+.es-p40r {
+	padding-right:40px;
+}
+.es-menu td {
+	border:0;
+}
+a {
+	font-family:lato, "helvetica neue", helvetica, arial, sans-serif;
+	font-size:18px;
+	text-decoration:underline;
+}
+h1 {
+	font-size:48px;
+	font-style:normal;
+	font-weight:normal;
+	color:#111111;
+}
+h1 a {
+	font-size:48px;
+}
+h2 {
+	font-size:24px;
+	font-style:normal;
+	font-weight:normal;
+	color:#111111;
+}
+h2 a {
+	font-size:24px;
+}
+h3 {
+	font-size:20px;
+	font-style:normal;
+	font-weight:normal;
+	color:#111111;
+}
+h3 a {
+	font-size:20px;
+}
+p, ul li, ol li {
+	font-size:18px;
+	font-family:lato, "helvetica neue", helvetica, arial, sans-serif;
+	line-height:150%;
+}
+ul li, ol li {
+	Margin-bottom:15px;
+}
+.es-menu td a {
+	text-decoration:none;
+	display:block;
+}
+.es-menu amp-img, .es-button amp-img {
+	vertical-align:middle;
+}
+.es-wrapper {
+	width:100%;
+	height:100%;
+}
+.es-wrapper-color {
+	background-color:#F4F4F4;
+}
+.es-content-body {
+	background-color:#FFFFFF;
+}
+.es-content-body p, .es-content-body ul li, .es-content-body ol li {
+	color:#666666;
+}
+.es-content-body a {
+	color:#EC6D64;
+}
+.es-header {
+	background-color:#EC6D64;
+}
+.es-header-body {
+	background-color:#EC6D64;
+}
+.es-header-body p, .es-header-body ul li, .es-header-body ol li {
+	color:#666666;
+	font-size:14px;
+}
+.es-header-body a {
+	color:#111111;
+	font-size:14px;
+}
+.es-footer {
+	background-color:transparent;
+}
+.es-footer-body {
+	background-color:transparent;
+}
+.es-footer-body p, .es-footer-body ul li, .es-footer-body ol li {
+	color:#666666;
+	font-size:14px;
+}
+.es-footer-body a {
+	color:#111111;
+	font-size:14px;
+}
+.es-infoblock, .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li {
+	line-height:120%;
+	font-size:12px;
+	color:#CCCCCC;
+}
+.es-infoblock a {
+	font-size:12px;
+	color:#CCCCCC;
+}
+a.es-button {
+	border-style:solid;
+	border-color:#EC6D64;
+	border-width:15px 25px 15px 25px;
+	display:inline-block;
+	background:#EC6D64;
+	border-radius:2px;
+	font-size:20px;
+	font-family:helvetica, "helvetica neue", arial, verdana, sans-serif;
+	font-weight:normal;
+	font-style:normal;
+	line-height:120%;
+	color:#FFFFFF;
+	text-decoration:none;
+	width:auto;
+	text-align:center;
+}
+.es-button-border {
+	border-style:solid solid solid solid;
+	border-color:#EC6D64 #EC6D64 #EC6D64 #EC6D64;
+	background:#EC6D64;
+	border-width:1px 1px 1px 1px;
+	display:inline-block;
+	border-radius:2px;
+	width:auto;
+}
+.es-button img {
+	display:inline-block;
+	vertical-align:middle;
+}
+.es-p-default {
+	padding-top:20px;
+	padding-right:30px;
+	padding-bottom:0px;
+	padding-left:30px;
+}
+.es-p-all-default {
+	padding:0px;
+}
+</style> 
+ </head> 
+ <body> 
+  <div class="es-wrapper-color"> 
+   <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0"> 
+     <tr class="gmail-fix" height="0"> 
+      <td> 
+       <table width="600" cellspacing="0" cellpadding="0" border="0" align="center"> 
+         <tr> 
+          <td style="line-height: 1px;min-width: 600px" height="0"><amp-anim src="https://esputnik.com/repository/applications/images/blank.gif" style="display: block;max-height: 0px;min-height: 0px;min-width: 600px;width: 600px" alt width="600" height="1" ></amp-anim></td> 
+         </tr> 
+       </table></td> 
+     </tr> 
+     <tr> 
+      <td valign="top"> 
+       <table cellpadding="0" cellspacing="0" class="es-header" align="center"> 
+         <tr> 
+          <td style="background-color: #1a535c" bgcolor="#1a535c" align="center"> 
+           <table class="es-header-body" style="background-color: #1a535c" width="600" bgcolor="#1a535c" align="center" cellspacing="0" cellpadding="0"> 
+             <tr> 
+              <td class="es-p10" align="left"> 
+               <table width="100%" cellspacing="0" cellpadding="0"> 
+                 <tr> 
+                  <td width="580" valign="top" align="center"> 
+                   <table width="100%" cellspacing="0" cellpadding="0" role="presentation"> 
+                     <tr> 
+                        <td align="center" style="font-size: 0px"><<img src="https://i.postimg.cc/y8X0SYTy/93921597262741196.png" alt style="display: block;" width="560" height="132"></img></td>                     </tr> 
+                    </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+           </table></td> 
+         </tr> 
+       </table> 
+       <table class="es-content" cellspacing="0" cellpadding="0" align="center"> 
+         <tr> 
+          <td align="center"> 
+           <table class="es-content-body" style="background-color: #ffffff" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center"> 
+             <tr> 
+              <td align="left"> 
+               <table width="100%" cellspacing="0" cellpadding="0"> 
+                 <tr> 
+                  <td width="600" valign="top" align="center"> 
+                   <table style="background-color: #ffffff" width="100%" cellspacing="0" cellpadding="0" bgcolor="#ffffff" role="presentation"> 
+                     <tr> 
+                      <td class="es-m-txt-l es-p15t es-p15b es-p30r es-p30l" bgcolor="#ffffff"><p style="line-height: 150%;font-family: roboto, 'helvetica neue', helvetica, arial, sans-serif">Estimado/a&nbsp;<strong>{{$details['destinatario']}}</strong> , <strong>{{$details['autor']}} </strong>perteneciente a la escuela <strong>{{$details['escuela']}}</strong>&nbsp;desea comunicarle lo siguiente:</p><p style="line-height: 150%;font-family: roboto, 'helvetica neue', helvetica, arial, sans-serif;text-align: justify"><br><strong>ASUNTO: {{$details['motivo']}}</strong></p></td> 
+                     </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+           </table></td> 
+         </tr> 
+       </table> 
+       <table class="es-content" cellspacing="0" cellpadding="0" align="center"> 
+         <tr> 
+          <td align="center"> 
+           <table class="es-content-body" style="background-color: transparent" width="600" cellspacing="0" cellpadding="0" align="center"> 
+             <tr> 
+              <td align="left"> 
+               <table width="100%" cellspacing="0" cellpadding="0"> 
+                 <tr> 
+                  <td width="600" valign="top" align="center"> 
+                   <table width="100%" cellspacing="0" cellpadding="0" role="presentation"> 
+                     <tr> 
+                      <td align="left" bgcolor="#ffffff"><p><br></p></td> 
+                     </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+           </table></td> 
+         </tr> 
+       </table> 
+       <table class="es-content" cellspacing="0" cellpadding="0" align="center"> 
+         <tr> 
+          <td align="center"> 
+           <table class="es-content-body" style="background-color: transparent" width="600" cellspacing="0" cellpadding="0" align="center"> 
+             <tr> 
+              <td class="es-p30r es-p30l" align="left" bgcolor="#ffffff" style="background-color: #ffffff"> 
+               <table cellpadding="0" cellspacing="0" width="100%"> 
+                 <tr> 
+                  <td width="540" align="center" valign="top"> 
+                   <table cellpadding="0" cellspacing="0" width="100%" role="presentation"> 
+                     <tr> 
+                      <td class="es-m-txt-l es-p5t es-p30r es-p30l" bgcolor="#4ecdc4" align="left"><h2 style="color: #666666;font-size: 18px;text-align: center"><b>- DETALLE&nbsp;📪 -</b></h2></td> 
+                     </tr> 
+                     <tr> 
+                      <td align="left" bgcolor="#ffffff"><p style="font-size: 16px"><h1>{{$details['descripcion']}}</h1><br><br></p></td> 
+                     </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+             <tr> 
+              <td class="es-p30r es-p30l" align="left" bgcolor="#ffffff" style="background-color: #ffffff"> 
+               <table cellpadding="0" cellspacing="0" width="100%"> 
+                 <tr> 
+                  <td width="540" align="center" valign="top"> 
+                   <table cellpadding="0" cellspacing="0" width="100%" role="presentation"> 
+                     <tr> 
+                      <td align="left" bgcolor="#ffffff"><p><br></p></td> 
+                     </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+             <tr> 
+              <td class="es-p5t es-p10b es-p15r" align="left" bgcolor="#1A535C" style="background-color: #1a535c"> 
+               <table cellspacing="0" cellpadding="0" width="100%"> 
+                 <tr> 
+                  <td width="585" valign="top" align="center"> 
+                   <table width="100%" cellspacing="0" cellpadding="0" role="presentation"> 
+                     <tr> 
+                      <td align="right"><p style="font-family: roboto, 'helvetica neue', helvetica, arial, sans-serif;line-height: 20px;color: #f7fff7;font-size: 13px"><br>Sistema de Gestión de Ayudantes de la Universidad de Talca.<br>Camino a Los Niches Km, 1, Curicó, Maule.</p></td> 
+                     </tr> 
+                   </table></td> 
+                 </tr> 
+               </table></td> 
+             </tr> 
+           </table></td> 
+         </tr> 
+       </table></td> 
+     </tr> 
+   </table> 
+  </div>  
+ </body>
+</html>
